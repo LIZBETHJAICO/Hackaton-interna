f7bd90dba0ee35a2bafe30b081f9f43e6adeb0d9
LIZBETHJAICO <lizbethjaico@gmail.com>
2020-04-17 14:57:11 -0500

    Updates

---
+++ precache-manifest.7d12d19d42e2ef912d675a35ba416996.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9687872bd315c22b646abc3f253659",
+    "url": "/beatuty-corp/index.html"
+  },
+  {
+    "revision": "70858dc4ff6b9747ccc7",
+    "url": "/beatuty-corp/static/css/2.b328fd89.chunk.css"
+  },
+  {
+    "revision": "cc2ece56b8bf8bd58379",
+    "url": "/beatuty-corp/static/css/main.dce76e45.chunk.css"
+  },
+  {
+    "revision": "70858dc4ff6b9747ccc7",
+    "url": "/beatuty-corp/static/js/2.f04835f5.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/beatuty-corp/static/js/2.f04835f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2ece56b8bf8bd58379",
+    "url": "/beatuty-corp/static/js/main.4ce4d347.chunk.js"
+  },
+  {
+    "revision": "5178da7c9d8a0b27e132",
+    "url": "/beatuty-corp/static/js/runtime-main.8b8d9263.js"
+  },
+  {
+    "revision": "e12c691f9a2cf34972aff23e88f77bda",
+    "url": "/beatuty-corp/static/media/arrow-left.e12c691f.svg"
+  },
+  {
+    "revision": "ddfa49b59d10e2837332c25cdd3931d1",
+    "url": "/beatuty-corp/static/media/asesoras.ddfa49b5.png"
+  },
+  {
+    "revision": "d5d8b12d7ff1aaa5c9215eb381ca1ea4",
+    "url": "/beatuty-corp/static/media/chicabeautycorp.d5d8b12d.jpg"
+  },
+  {
+    "revision": "a7bea1e1ed77a2b27bb30eb5e9d67fed",
+    "url": "/beatuty-corp/static/media/facebook.a7bea1e1.svg"
+  },
+  {
+    "revision": "68fb603f18b33dd710798356681acda4",
+    "url": "/beatuty-corp/static/media/facebook2.68fb603f.svg"
+  },
+  {
+    "revision": "247e377145a5e6392581ad696f6314b3",
+    "url": "/beatuty-corp/static/media/google.247e3771.svg"
+  },
+  {
+    "revision": "e16f228d4e00e5ef81de26115efb3ea3",
+    "url": "/beatuty-corp/static/media/logobeautycorp.e16f228d.png"
+  },
+  {
+    "revision": "02cda9bfbefee23ac8ac2b756af5bafb",
+    "url": "/beatuty-corp/static/media/logocabeza.02cda9bf.png"
+  },
+  {
+    "revision": "cb41c8c0eef6a0a6207c7d5398af046a",
+    "url": "/beatuty-corp/static/media/logoletra.cb41c8c0.png"
+  },
+  {
+    "revision": "3f631b75ac5114f7ac015cc070716b01",
+    "url": "/beatuty-corp/static/media/logotipo-de-instagram.3f631b75.svg"
+  },
+  {
+    "revision": "54dda2f07f661e62644dea2a1163ccb1",
+    "url": "/beatuty-corp/static/media/minus.54dda2f0.svg"
+  },
+  {
+    "revision": "f64eea8aba8fe7b7a85ec6b4d19a61f1",
+    "url": "/beatuty-corp/static/media/moda.f64eea8a.svg"
+  },
+  {
+    "revision": "45b94e4a9959fc3ba27f1118eaf2cc15",
+    "url": "/beatuty-corp/static/media/plus.45b94e4a.svg"
+  },
+  {
+    "revision": "0919a21419098325a1e6fb2af1be23a8",
+    "url": "/beatuty-corp/static/media/search.0919a214.svg"
+  },
+  {
+    "revision": "3a91ec41f20701b80f4cbc30d03ed408",
+    "url": "/beatuty-corp/static/media/send-right.3a91ec41.svg"
+  },
+  {
+    "revision": "df1d4b10ffde8ee232af9cdd9fb09ba9",
+    "url": "/beatuty-corp/static/media/store.df1d4b10.svg"
+  },
+  {
+    "revision": "d0abca0c825490c2fbf742fb6ac71615",
+    "url": "/beatuty-corp/static/media/twitter.d0abca0c.svg"
+  },
+  {
+    "revision": "a07f31ec1e5d760b41882dc90756aa01",
+    "url": "/beatuty-corp/static/media/user.a07f31ec.svg"
+  },
+  {
+    "revision": "830538bf5f80c72201f3bc72d9b93436",
+    "url": "/beatuty-corp/static/media/van.830538bf.svg"
+  },
+  {
+    "revision": "76d65d7839fda10e5f17c76d43bd4680",
+    "url": "/beatuty-corp/static/media/vector.76d65d78.svg"
+  },
+  {
+    "revision": "1f27a0fc9b817b3d3d23f1230c34f74a",
+    "url": "/beatuty-corp/static/media/youtube.1f27a0fc.svg"
+  }
+]);
